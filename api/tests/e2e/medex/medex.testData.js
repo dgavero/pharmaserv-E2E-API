83880b1503e2deda91a8fce100bbce3eed5f464f
davetheG <dtopdave1997@gmail.com>
2026-05-08 13:04:23 +0800

    added medex5

--- api/tests/e2e/medex/medex.testData.js
+++ api/tests/e2e/medex/medex.testData.js
@@ -5,6 +5,7 @@ export const MEDEX_RX_ATTACHMENT_IMAGE_PATH = 'upload/images/prescription1.png';
 export const MEDEX_RX_PRESCRIPTION_IMAGE_PATH = 'upload/images/prescription2.png';
 export const MEDEX_SENIOR_CARD_FRONT_IMAGE_PATH = 'upload/api-images/senior-id-front.png';
 export const MEDEX_SENIOR_CARD_BACK_IMAGE_PATH = 'upload/api-images/senior-id-back.png';
+export const MEDEX_SENIOR_RX_ATTACHMENT_IMAGE_PATH = 'upload/api-images/senior-rx-img.png';
 export const MEDEX_PROOF_OF_PAYMENT_IMAGE_PATH = 'upload/images/proof1.png';
 export const MEDEX_PICKUP_PROOF_IMAGE_PATH = 'upload/images/proofOfPickup.png';
 export const MEDEX_DELIVERY_PROOF_IMAGE_PATH = 'upload/images/proofOfDelivery.png';
